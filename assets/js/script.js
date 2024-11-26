a97ffc26341675e00346587fdf4b032a341815c7
ikealmighty <isaacyates7@gmail.com>
2024-11-25 18:41:05 -0600

    created getCartItemsFromLocalStorage and addCartItemToLocalStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+function getCartItemsFromLocalStorage() {
+    // returns a list of cart objects, or an empty list if the cart list hasn't been set in localStorage
+    return JSON.parse(localStorage.getItem("cart") || "[]");
+}
 
+function addCartItemToLocalStorage(pastryOrTeaItem) {
+    let cartItems = getCartItemsFromLocalStorage();
+
+    // use the spread operator to add the new item to the list
+    cartItems = [...cartItems, pastryOrTeaItem];
+
+    // store new list in localStorage under 'cart', overwriting the old list if it exists.
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+}
